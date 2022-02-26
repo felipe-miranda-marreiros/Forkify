37dc16bbfdf524417408b4b4aeb927a42cb491fd
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-02-25 21:24:20 -0300

    Renderizando receitas com template literals e animações

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,7 @@
+import icons from 'url:../img/icons.svg';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const { async } = require('regenerator-runtime');
 
 const recipeContainer = document.querySelector('.recipe');
@@ -14,6 +18,24 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+//////////////////////////////////////////////////////////
+/**
+ *
+ * @param {html} parentEl recebe o parente mais próximo e coloca uma animação de "carregando" antes de qualquer coisa aparecer.
+ */
+const renderSpinner = function (parentEl) {
+  const markup = `
+        <div class="spinner">
+          <svg>
+            <use href="${icons}#icon-loader"></use>
+          </svg>
+        </div>
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+///////////////////////////////////////////////////////////
+
 /**
  * A função showRecipe é uma async function que retorna um fetch de uma API.
  * Nela temos, Try, Catch
@@ -23,9 +45,13 @@ const showRecipe = async function () {
     /**
      * Guardamos o conteúdo da API em uma variável chamada response. O fetch é precedido de await.
      */
+    //////////////////////////////////////
+    //Colocamos a função renderSpinner antes do fetch para criar a impressão de carregamento
+    renderSpinner(recipeContainer);
+    //////////////////////////////////////
     const response = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca36'
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca36'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
     /**
      * Formatamos o conteúdo do fetch (que vem em string), em formato json(), conforme a variável abaixo.
@@ -58,6 +84,108 @@ const showRecipe = async function () {
       ingredients: recipe.ingredients,
     };
 
+    //Renderizando receitas abaixo
+
+    //Pegando informações da variável recipe
+    const markup = `
+    <figure class="recipe__fig">
+          <img src="${recipe.image}" alt="${
+      recipe.title
+    }" class="recipe__img" />
+          <h1 class="recipe__title">
+            <span>${recipe.title}</span>
+          </h1>
+        </figure>
+
+        <div class="recipe__details">
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="${icons}#icon-clock"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--minutes">${
+              recipe.cookingTime
+            }</span>
+            <span class="recipe__info-text">minutes</span>
+          </div>
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="${icons}#icon-users"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--people">${
+              recipe.servings
+            }</span>
+            <span class="recipe__info-text">servings</span>
+
+            <div class="recipe__info-buttons">
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="${icons}#icon-minus-circle"></use>
+                </svg>
+              </button>
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="${icons}#icon-plus-circle"></use>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div class="recipe__user-generated">
+            <svg>
+              <use href="${icons}#icon-user"></use>
+            </svg>
+          </div>
+          <button class="btn--round">
+            <svg class="">
+              <use href="${icons}#icon-bookmark-fill"></use>
+            </svg>
+          </button>
+        </div>
+
+        <div class="recipe__ingredients">
+          <h2 class="heading--2">Recipe ingredients</h2>
+          <ul class="recipe__ingredient-list">
+          ${recipe.ingredients
+            .map(ingredients => {
+              return `
+            <li class="recipe__ingredient">
+                <svg class="recipe__icon">
+                <use href="${icons}#icon-check"></use>
+                </svg>
+                <div class="recipe__quantity">${ingredients.quantity}</div>
+                <div class="recipe__description">
+                <span class="recipe__unit">${ingredients.unit}</span>
+                ${ingredients.description}
+              </div>
+           </li>
+            `;
+            })
+            .join('')}
+        </div>
+
+        <div class="recipe__directions">
+          <h2 class="heading--2">How to cook it</h2>
+          <p class="recipe__directions-text">
+            This recipe was carefully designed and tested by
+            <span class="recipe__publisher">${
+              recipe.publisher
+            }</span>. Please check out
+            directions at their website.
+          </p>
+          <a
+            class="btn--small recipe__btn"
+            href="${recipe.sourceUrl}"
+            target="_blank"
+          >
+            <span>Directions</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+          </a>
+        </div>
+    `;
+    recipeContainer.innerHTML = '';
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
     /**
      * A função de catch é detectar qualquer erro apresentado no try.
      */
