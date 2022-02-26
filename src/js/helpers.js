cd2e32d9b0dbc92ac3539697f2f1d892a8db47d4
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-02-26 10:55:34 -0300

    Helpers, Configuration Files

---
+++ src/js/helpers.js
@@ -0,0 +1,35 @@
+import { TIMEOUT_SEC } from './config';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+export const getJSON = async function (url) {
+  try {
+    /**
+     * Guardamos o conteúdo da API em uma variável chamada response. O fetch é precedido de await.
+     */
+    const response = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
+    /**
+     * Formatamos o conteúdo do fetch (que vem em string), em formato json(), conforme a variável abaixo.
+     */
+    const data = await response.json();
+
+    /**
+     * Se o conteúdo do response tiver algum erro, nós usamos o if abaixo para mostrar ao usuário o conteúdo do erro.
+     *
+     * response.ok é um valor booleano que apresenta true ou false. True significa que a conexão foi feita com sucesso e False ocorreu algum erro.
+     *
+     * data.message é o status do conteúdo da API.
+     *
+     */
+    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
--- src/js/model.js
+++ src/js/model.js
@@ -1,3 +1,5 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
 export const state = {
   recipe: {},
 };
@@ -8,27 +10,7 @@ export const state = {
  */
 export const loadRecipe = async function (id) {
   try {
-    /**
-     * Guardamos o conteúdo da API em uma variável chamada response. O fetch é precedido de await.
-     */
-    const response = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca36'
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    /**
-     * Formatamos o conteúdo do fetch (que vem em string), em formato json(), conforme a variável abaixo.
-     */
-    const data = await response.json();
-
-    /**
-     * Se o conteúdo do response tiver algum erro, nós usamos o if abaixo para mostrar ao usuário o conteúdo do erro.
-     *
-     * response.ok é um valor booleano que apresenta true ou false. True significa que a conexão foi feita com sucesso e False ocorreu algum erro.
-     *
-     * data.message é o status do conteúdo da API.
-     *
-     */
-    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     /**
      * O objeto abaixo é uma forma de formatação da data recebida pelo fetch. É só uma reorganização de nome. Recipe faz parte da data.data.
@@ -48,6 +30,6 @@ export const loadRecipe = async function (id) {
 
     console.log(state.recipe);
   } catch (error) {
-    alert(error);
+    console.error(error);
   }
 };
