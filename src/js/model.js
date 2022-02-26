ea3ec98a41a83256ab26e8aaef338dab6e17d158
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-02-26 14:36:29 -0300

    Propagando erro para outro Promise

--- src/js/model.js
+++ src/js/model.js
@@ -27,9 +27,7 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-
-    console.log(state.recipe);
   } catch (error) {
-    console.error(error);
+    throw error;
   }
 };
